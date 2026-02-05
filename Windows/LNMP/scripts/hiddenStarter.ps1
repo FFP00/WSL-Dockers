5cf5d829e09b2b88e4b5daf398069501fad3cb10
FFP00 <FFP00@iesemilidarder.com>
2026-02-05 06:35:45 +0100

    "LNMP y NODE precooked"

---
+++ Windows/LNMP/scripts/hiddenStarter.ps1
@@ -0,0 +1,16 @@
+# Nombre del compose entre comillas
+$COMPOSE_NAME = "lnmp_stack"
+
+
+
+    # Obtiene la ruta desde donde se está llama actualmente a este script
+    $ruta = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+    # Montamos la ruta completa añadiendo el script containerLifter al final 
+    $rutaLifter = Join-Path $ruta "containerLifter.ps1"
+
+    # Corre el script en base a la ruta que hemos creado antes
+    Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File `"$rutaLifter`"" -Wait
+
+    # Cuando acaba el script hace un node compose down
+    docker compose -p $COMPOSE_NAME down
---
+++ Windows/NODE/scripts/containerLifter.ps1
@@ -0,0 +1,7 @@
+# Especificamod la ruta de donde estará el script .sh para iniciar los contenedores
+$RUTA = "/home/netti/dockers/NODE"
+
+
+
+    # Corre el script dentro del entorno WSL
+    wsl -d Debian bash -lc "cd '$RUTA' && ./lifter.sh"
