e320a9616022d0cfb97b45a8308309825a9227cc
FFP00 <FFP00@iesemilidarder.com>
2026-02-05 06:15:31 +0100

    "Cambios para ser funcional con WSL"

---
+++ Linux/LNMP/lifter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+COMPOSE_NAME="lnmp_stack"
+CONTAINER_SHELL="app_php"
+
+
+    #Crea workdir por si no existe
+    mkdir -p workdir
+
+    # Abrimos Visual Studio Code
+    code .
+
+    # Hacer un docker compose up
+    docker compose -p "$COMPOSE_NAME" up --build -d
+
+    # Entrar al contenedor principal
+    docker exec -it "$CONTAINER_SHELL" sh -l
+
+    # Salir
+    exit
+
